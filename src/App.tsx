9429a490c71e747f00226246a993a5aa3b39851f
benfanderson <benfanderson81@gmail.com>
2021-07-15 20:54:54 +0800

    Removed combox options

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
-import fastfood from "./fastfood.json"
 import './styles.scss';
 
 const App: React.FC<unknown> = () => {
   const [weight, setWeight] = useState<string>('');
   const [kjConsumed, setkjConsumed] = useState<string>('');
   const [walkTime, setWalkTime] = useState<string>('');
-  const [kjBurnedPerkj, setkjBurnedPerkj] = useState<number>(11.06);
-  const fastFood = fastfood.list;
+  const [kjBurnedPerkg, setkjBurnedPerkg] = useState<number>(11.06);
 
   const handleClick = (event: { preventDefault: () => void; }) => {
     if(kjConsumed.length === 0 || weight.length === 0) {
@@ -15,16 +13,33 @@ const App: React.FC<unknown> = () => {
     }
     const weightInt = parseInt(weight)
     const kjInt = parseInt(kjConsumed);
-    const kjBurned = kjInt/(weightInt*kjBurnedPerkj)
-    setWalkTime(`It would take you ${time_convert(kjBurned)} to burn off ${kjConsumed} kilojoules`)
+    const kjBurned = kjInt/(weightInt*kjBurnedPerkg)
+    setWalkTime(`It would take you ${time_convert(kjBurned, kjBurnedPerkg)} to burn off ${kjConsumed} kilojoules`)
     event.preventDefault();
   };
 
-  const time_convert = (num: number) => { 
-    const decimalTimeString = num;
-    const n = new Date(0,0);
-    n.setMinutes(+decimalTimeString * 60);
-    return n.toTimeString().slice(0, 5);
+  // const time_convert = (num: number) => { 
+  //   const decimalTimeString = num;
+  //   const n = new Date(0,0);
+  //   n.setMinutes(+decimalTimeString * 60);
+  //   return n.toTimeString().slice(0, 5);
+  // }
+
+  const time_convert = (kjBurned: number, kjBurnedPerkg: number ) => { 
+    let distance;
+    if (kjBurnedPerkg === 11.06) {
+      distance = kjBurned*3
+    } else if (kjBurnedPerkg === 14.74) {
+      distance = kjBurned*5
+    } else {
+      distance = kjBurned*6
+    }
+    // const decimalTimeString = num;
+    // const n = new Date(0,0);
+    // n.setHours(+decimalTimeString * 60);
+    // // return n.toTimeString().slice(0, 5);
+    // console.log(kjBurnedPerkg)
+    return distance
   }
   
   return (
@@ -38,8 +53,8 @@ const App: React.FC<unknown> = () => {
         <br/>
         <label className="form--label"><b>Walking speed</b></label> 
         <br/>
-          <select className="form--input" name="speed" id="speed" value={kjBurnedPerkj}
-            onChange={(event) => {setkjBurnedPerkj(event.target.value as unknown as number)}}>
+          <select className="form--input" name="speed" id="speed" value={kjBurnedPerkg}
+            onChange={(event) => {setkjBurnedPerkg(event.target.value as unknown as number)}}>
               <option value="11.06" >Slow - 3km/h</option>
               <option value="14.74" >Moderate - 5km/h</option>
               <option value="18.44">Brisk - 6km/h</option>
@@ -47,11 +62,7 @@ const App: React.FC<unknown> = () => {
         <br/>
         <label className="form--label"><b>How many kjs did you consume?</b></label>
         <br/>
-          <input id="kj_consumed" list="kilojoules-list" required className="form--input" 
-          value={kjConsumed} onChange={(event) => {setkjConsumed(event.target.value)}} />
-          <datalist id="kilojoules-list">
-            {fastFood.map((element, index) => <option  value={element.value} key={index}>{element.name}</option> )}
-          </datalist>
+        <input type="number" name="fkjs" className="form--input" id="kj_consumed" required value={kjConsumed} onChange={(event) => {setkjConsumed(event.target.value)}}  />
         <br/>
         <button onClick={handleClick}>
           Calculate walking time
